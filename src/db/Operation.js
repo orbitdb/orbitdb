8686317bf2a9eab29a25ffbfd479274f889cc83d
haad <haad@headbanggames.com>
2016-03-05 12:11:50 +0100

    Await for creating a DB operation

--- src/db/Operation.js
+++ src/db/Operation.js
@@ -11,8 +11,8 @@ class Operation {
   static create(ipfs, log, user, operation, key, value, data) {
     var createOperation = async(() => {
       return new Promise(async((resolve, reject) => {
-        const hash = Operation._createOperation(ipfs, user, operation, key, value);
-        const res = await(log.add(hash));
+        const hash = await(Operation._createOperation(ipfs, user, operation, key, value));
+        await(log.add(hash));
         const listHash = await(log.ipfsHash);
         resolve(listHash);
       }));
