d823ab4d2b218b258eeb291b8aee297ec754ec98
haad <haad@headbanggames.com>
2016-05-04 11:30:37 +0200

    Use reduce() instead of forEach() for building KeyValueIndex

--- src/stores/kvstore/KeyValueIndex.js
+++ src/stores/kvstore/KeyValueIndex.js
@@ -10,18 +10,17 @@ class KeyValueIndex {
   }
 
   updateIndex(oplog, updated) {
-    let handled = [];
-
-    updated.reverse().forEach((item) => {
+    updated.reverse().reduce((handled, item) => {
       if(handled.indexOf(item.key) === -1) {
         handled.push(item.key);
         if(item.op === 'PUT') {
           this._index[item.key] = item.value
-        } else if (item.op === 'DEL') {
+        } else if(item.op === 'DEL') {
           delete this._index[item.key];
         }
       }
-    });
+      return handled;
+    }, []);
   }
 }
 
