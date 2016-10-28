1b845fd74d5f25fc082b867e7f6bc96727742d53
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-10-28 12:30:46 +0200

    make it actually work

--- src/Cache.js
+++ src/Cache.js
@@ -2,27 +2,31 @@
 
 const pull = require('pull-stream')
 const BlobStore = require('fs-pull-blob-store')
+const Lock = require('lock')
 
 let filePath
 let store
 let cache = {}
+const lock = new Lock()
 
 class Cache {
   static set(key, value) {
     return new Promise((resolve, reject) => {
       cache[key] = value
       if(filePath && store) {
-        // console.log("write cache:", filePath, JSON.stringify(cache, null, 2))
-        pull(
-          pull.values([cache]),
-          pull.map((v) => JSON.stringify(v, null, 2)),
-          store.write(filePath, (err) => {
-            if (err) {
-              return reject(err)
-            }
-            resolve()
-          })
-        )
+        lock(filePath, (release) => {
+          // console.log("write cache:", filePath, JSON.stringify(cache, null, 2))
+          pull(
+            pull.values([cache]),
+            pull.map((v) => JSON.stringify(v, null, 2)),
+            store.write(filePath, release((err) => {
+              if (err) {
+                return reject(err)
+              }
+              resolve()
+            }))
+          )
+        })
       } else {
         resolve()
       }
@@ -36,6 +40,8 @@ class Cache {
   static loadCache(cacheFile = 'orbit-db.cache') {
     cache = {}
     store = new BlobStore(cacheFile)
+    filePath = cacheFile
+
     return new Promise((resolve, reject) => {
 
       // console.log("load cache:", cacheFile)
@@ -44,17 +50,20 @@ class Cache {
           return resolve()
         }
 
-        filePath = cacheFile
-        pull(
-          store.read(cacheFile),
-          pull.collect((err, res) => {
-            if (err) {
-              return reject(err)
-            }
+        lock(cacheFile, (release) => {
+          pull(
+            store.read(cacheFile),
+            pull.collect(release((err, res) => {
+              if (err) {
+                return reject(err)
+              }
 
-            resolve(JSON.parse(res[0].toString() || '{}'))
-          })
-        )
+              cache = JSON.parse(Buffer.concat(res).toString() || '{}')
+
+              resolve()
+            }))
+          )
+        })
       })
     })
   }
