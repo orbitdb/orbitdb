98041c9a90d8316251c54c1e7c97ad0e7764f199
haad <haad@haja.io>
2018-08-16 21:05:34 +0300

    Test only the previously failing test

--- test/multiple-databases.test.js
+++ test/multiple-databases.test.js
@@ -141,7 +141,7 @@ Object.keys(testAPIs).forEach(API => {
         await db.drop()
     })
 
-    it('replicates multiple open databases', async () => {
+    it.only('replicates multiple open databases', async () => {
       const entryCount = 32
       const entryArr = []
 
