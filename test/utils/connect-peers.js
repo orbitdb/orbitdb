c2bf30e374aab41b547bc605bf03cf6962fad532
haad <haad@haja.io>
2018-09-13 09:17:22 +0200

    Only call connect from on one peer

--- test/utils/connect-peers.js
+++ test/utils/connect-peers.js
@@ -7,13 +7,13 @@ const connectIpfsNodes = async (ipfs1, ipfs2) => {
   console.log("peer2.id>", id2)
   try {
     const a = await ipfs1.swarm.connect(id2.addresses[0])
-    const b = await ipfs2.swarm.connect(id1.addresses[0])
+    // const b = await ipfs2.swarm.connect(id1.addresses[0])
     console.log("swarm.peer1.connect>", a)
-    console.log("swarm.peer2.connect>", b)
+    // console.log("swarm.peer2.connect>", b)
     const peers = await ipfs1.swarm.peers()
     console.log("swarm.peer1.peers>", peers)
   } catch (e) {
-    console.error("ERROR!", e)
+    console.error("Error connecting nodes!", e)
   }
   console.log("connected!")
   // return new Promise(resolve => setTimeout(resolve, 1000))
