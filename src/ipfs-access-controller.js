553e302b9f70f6a6f4f26efb6f043cb1f14b84cd
Thiago Delgado <thiagodelgado111@gmail.com>
2018-10-30 16:54:29 +0100

    Clean up AccessController interface

--- src/ipfs-access-controller.js
+++ src/ipfs-access-controller.js
@@ -6,9 +6,45 @@ class IPFSAccessController extends AccessController {
   constructor (ipfs) {
     super()
     this._ipfs = ipfs
+    this._access = {
+      admin: [], // Not used atm
+      write: [],
+      read: [], // Not used atm
+    }
   }
 
-  async load (address) {
+  /* Properties */
+  get admin () {
+    return this._access.admin
+  }
+
+  get write () {
+    // Both admins and write keys can write
+    return this._access.write.concat(this._access.admin)
+  }
+
+  // Not used atm
+  get read () {
+    return this._access.read
+  }
+
+  async load (accessControllerAddress) {
+    return this.setup({ accessControllerAddress });
+  }
+
+  async save () {
+    let hash
+    try {
+      const access = JSON.stringify(this._access, null, 2)
+      const dag = await this._ipfs.object.put(new Buffer(access))
+      hash = dag.toJSON().multihash.toString()
+    } catch (e) {
+      console.log("ACCESS ERROR:", e)
+    }
+    return hash
+  }
+
+  async setup ({ accessControllerAddress: address }) {
     // Transform '/ipfs/QmPFtHi3cmfZerxtH9ySLdzpg1yFhocYDZgEZywdUXHxFU'
     // to 'QmPFtHi3cmfZerxtH9ySLdzpg1yFhocYDZgEZywdUXHxFU'
     if (address.indexOf('/ipfs') === 0)
@@ -23,16 +59,67 @@ class IPFSAccessController extends AccessController {
     }
   }
 
-  async save () {
-    let hash
-    try {
-      const access = JSON.stringify(this._access, null, 2)
-      const dag = await this._ipfs.object.put(new Buffer(access))
-      hash = dag.toJSON().multihash.toString()
-    } catch (e) {
-      console.log("ACCESS ERROR:", e)
+  async grant (access, key) {
+    // if(!Object.keys(this._access).includes(access))
+    //   throw new Error(`unknown access level: ${access}`)
+    // if (!this._access[access].includes(key))
+    //   this._access[access].push(key)
+
+    // TODO: uniques only
+    switch (access) {
+      case 'admin':
+        this._access.admin.push(key)
+        break
+      case 'write':
+        this._access.write.push(key)
+        break
+      case 'read':
+        this._access.read.push(key)
+        break
+      default:
+      break
     }
-    return hash
+  }
+
+  async revoke (access, key) {
+    const without = (arr, e) => {
+      const reducer = (res, val) => {
+        if (val !== key)
+          res.push(val)
+        return res
+      }
+      return arr.reduce(reducer, [])
+    }
+
+    // if(!Object.keys(this._access).includes(access))
+    //   throw new Error(`unknown access level: ${access}`)
+    // if (this._access[access].includes(key))
+    //   this._access[access] = without(this._access[access], key)
+
+    switch (access) {
+      case 'admin':
+        this._access.admin = without(this._access.admin, key)
+        break
+      case 'write':
+        this._access.write = without(this._access.write, key)
+        break
+      case 'read':
+        this._access.read = without(this._access.read, key)
+        break
+      default:
+      break
+    }
+  }
+
+  async canAppend(entry, identityProvider) {
+    //verify identity?
+    if (this._access.write.includes('*'))
+      return true
+
+    if (this._access.write.includes(entry.identity.publicKey))
+      return true
+
+    return false
   }
 }
 
