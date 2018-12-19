edd3fd6c5b9cac8ba4bf94efb57d0f6b2c52dce2
haad <haad@haja.io>
2018-12-19 15:53:24 +0100

    Fix createDBManifest hashing mismatch

--- src/db-manifest.js
+++ src/db-manifest.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const { DAGNode, util } = require('ipld-dag-pb')
 
 // Creates a DB manifest file and saves it in IPFS
 const createDBManifest = async (ipfs, name, type, accessControllerAddress, onlyHash) => {
@@ -8,39 +7,14 @@ const createDBManifest = async (ipfs, name, type, accessControllerAddress, onlyH
     type: type,
     accessController: path.join('/ipfs', accessControllerAddress),
   }
-  let cid, hash
-  const manifestJSON = JSON.stringify(manifest)
-  if (onlyHash) {
-    // const dag = await new Promise(resolve => {
-    //   DAGNode.create(Buffer.from(manifestJSON), (err, n) => {
-    //     if (err) {
-    //       throw err
-    //     }
-    //     resolve(n)
-    //   })
-    // })
-    cid = await new Promise(resolve => {
-      // util.serialize(manifestJSON), (err, node) => {
-      //   if (err) {
-      //     throw err
-      //   }
-      //   util.cid(node, { version: 1 }, (err, node) => {
-      //     if (err) {
-      //       throw err
-      //     }
-      //     resolve(node)
-      //   })
-      // })
-      util.cid(manifestJSON, { version: 1 }, (err, node) => {
-        if (err) {
-          throw err
-        }
-        resolve(node)
-      })
-    })
-  } else {
-    cid = await ipfs.dag.put(Buffer.from(manifestJSON))
-  }
+
+  const data = Buffer.from(JSON.stringify(manifest))
+
+  const cid = await ipfs.dag.put(data, { 
+    version: 1, 
+    onlyHash: onlyHash || false 
+  })
+
   return cid.toBaseEncodedString()
 }
 
