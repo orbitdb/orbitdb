8e4f22e0f3494f6ad78ad89715c628c086c29cad
shamb0t <shams.methnani@gmail.com>
2019-02-20 13:18:11 +0000

    Update conf

--- conf/webpack.example.config.js
+++ conf/webpack.example.config.js
@@ -2,19 +2,11 @@
 
 const path = require('path')
 const webpack = require('webpack')
-const Uglify = require('uglifyjs-webpack-plugin')
-
-const uglifyOptions = {
-  uglifyOptions: {
-    mangle: false,
-    compress: false,
-  },
-}
 
 module.exports = {
   entry: './examples/browser/browser-webpack-example/index.js',
   output: {
-    filename: './examples/browser/browser-webpack-example/bundle.js'
+    filename: '../examples/browser/browser-webpack-example/bundle.js'
   },
   target: 'web',
   devtool: 'none',
@@ -26,8 +18,7 @@ module.exports = {
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
       }
-    }),
-    new Uglify(uglifyOptions),
+    })
   ],
   resolve: {
     modules: [
