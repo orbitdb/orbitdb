85d709a9577e3ecb0c0dad6081459321e6f6a6c1
anders <andersb@tuta.io>
2019-09-10 14:06:45 -0500

    adds optional meta field to manifest

--- src/db-manifest.js
+++ src/db-manifest.js
@@ -6,7 +6,9 @@ const createDBManifest = async (ipfs, name, type, accessControllerAddress, optio
   const manifest = {
     name: name,
     type: type,
-    accessController: path.join('/ipfs', accessControllerAddress)
+    accessController: path.join('/ipfs', accessControllerAddress),
+    // meta field is only added to manifest if options.meta is defined
+    ...(options.meta !== undefined ? { meta: options.meta } : {})
   }
 
   return io.write(ipfs, options.format || 'dag-cbor', manifest, options)
