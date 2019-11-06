1838d093bc46f1b317d1311328e556d237f791d7
hazæ41 <hazae41@gmail.com>
2019-11-06 21:25:02 +0100

    Expose modules in OrbitDB object
    so we can grab them from OrbitDB without reimporting them.
    
    Related to https://github.com/orbitdb/orbit-db/issues/715

--- src/OrbitDB.js
+++ src/OrbitDB.js
@@ -56,6 +56,13 @@ class OrbitDB {
     // testing with orbit-db-access-controller
     AccessControllers = options.AccessControllers || AccessControllers
   }
+  
+  static get Pubsub () { return Pubsub; }
+  static get Cache () { return Cache; }
+  static get Keystore () { return Keystore; }
+  static get Identities () { return Identities; }
+  static get AccessControllers () { return AccessControllers; }
+  static get Storage () { return Storage; }
 
   get cache () { return this.caches[this.directory].cache }
 
