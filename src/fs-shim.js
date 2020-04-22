89ff063f731acb105763aa35a7d38924614553f3
Kia Rahimian <kia@tintmail.com>
2020-04-22 12:18:04 -0400

    fix: fs shim when running on electron renderer process

--- src/fs-shim.js
+++ src/fs-shim.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-const fs = (typeof window === 'object' || typeof self === 'object') ? null
-  : eval('require("fs")')
+const fs = (typeof process !== 'object' && (typeof window === 'object' || typeof self === 'object')) ? null : eval('require("fs")')
 
 module.exports = fs
