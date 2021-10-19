04ef88ba801a1c1dceda299fc5ef1dba05a5a027
haad <haad@headbanggames.com>
2021-10-19 15:41:39 +0300

    Run persistency tests also

--- test/persistency.js
+++ test/persistency.js
@@ -34,7 +34,7 @@ const tests = [
 
 Object.keys(testAPIs).forEach(API => {
   tests.forEach(test => {
-    describe(`orbit-db - ${test.title} (${API})`, function() {
+    describe.only(`orbit-db - ${test.title} (${API})`, function() {
       this.timeout(config.timeout)
 
       const entryCount = 65
