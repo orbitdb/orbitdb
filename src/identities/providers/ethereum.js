739ddc9ded1c92bda70390c074e4e0ba4a256dab
haad <haad@headbanggames.com>
2023-03-02 08:31:55 +0200

    Disable Ethereum and DID identity providers for now

--- src/identities/providers/ethereum.js
+++ src/identities/providers/ethereum.js
@@ -1,70 +1,70 @@
-import IdentityProvider from './interface.js'
-import { Wallet, verifyMessage } from '@ethersproject/wallet'
+// import IdentityProvider from './interface.js'
+// import { Wallet, verifyMessage } from '@ethersproject/wallet'
 
-const type = 'ethereum'
+// const type = 'ethereum'
 
-class EthIdentityProvider extends IdentityProvider {
-  constructor ({ wallet } = {}) {
-    super()
-    this.wallet = wallet
-  }
+// class EthIdentityProvider extends IdentityProvider {
+//   constructor ({ wallet } = {}) {
+//     super()
+//     this.wallet = wallet
+//   }
 
-  // Returns the type of the identity provider
-  static get type () { return type }
+//   // Returns the type of the identity provider
+//   static get type () { return type }
 
-  // Returns the signer's id
-  async getId (options = {}) {
-    if (!this.wallet) {
-      this.wallet = await this._createWallet(options)
-    }
-    return this.wallet.getAddress()
-  }
+//   // Returns the signer's id
+//   async getId (options = {}) {
+//     if (!this.wallet) {
+//       this.wallet = await this._createWallet(options)
+//     }
+//     return this.wallet.getAddress()
+//   }
 
-  // Returns a signature of pubkeysignature
-  async signIdentity (data) {
-    const wallet = this.wallet
+//   // Returns a signature of pubkeysignature
+//   async signIdentity (data) {
+//     const wallet = this.wallet
 
-    if (!wallet) {
-      throw new Error('wallet is required')
-    }
+//     if (!wallet) {
+//       throw new Error('wallet is required')
+//     }
 
-    return wallet.signMessage(data)
-  }
+//     return wallet.signMessage(data)
+//   }
 
-  static async verifyIdentity (identity) {
-    // Verify that identity was signed by the id
-    const signerAddress = verifyMessage(
-      identity.publicKey + identity.signatures.id,
-      identity.signatures.publicKey
-    )
-    return (signerAddress === identity.id)
-  }
+//   static async verifyIdentity (identity) {
+//     // Verify that identity was signed by the id
+//     const signerAddress = verifyMessage(
+//       identity.publicKey + identity.signatures.id,
+//       identity.signatures.publicKey
+//     )
+//     return (signerAddress === identity.id)
+//   }
 
-  async _createWallet (options = {}) {
-    if (options.mnemonicOpts) {
-      if (!options.mnemonicOpts.mnemonic) {
-        throw new Error('mnemonic is required')
-      }
+//   async _createWallet (options = {}) {
+//     if (options.mnemonicOpts) {
+//       if (!options.mnemonicOpts.mnemonic) {
+//         throw new Error('mnemonic is required')
+//       }
 
-      const { mnemonic, path, wordlist } = options.mnemonicOpts
-      return Wallet.fromMnemonic(mnemonic, path, wordlist)
-    }
+//       const { mnemonic, path, wordlist } = options.mnemonicOpts
+//       return Wallet.fromMnemonic(mnemonic, path, wordlist)
+//     }
 
-    if (options.encryptedJsonOpts) {
-      if (!options.encryptedJsonOpts.json) {
-        throw new Error('encrypted json is required')
-      }
+//     if (options.encryptedJsonOpts) {
+//       if (!options.encryptedJsonOpts.json) {
+//         throw new Error('encrypted json is required')
+//       }
 
-      if (!options.encryptedJsonOpts.password) {
-        throw new Error('password for encrypted json is required')
-      }
+//       if (!options.encryptedJsonOpts.password) {
+//         throw new Error('password for encrypted json is required')
+//       }
 
-      const { json, password, progressCallback } = options.encryptedJsonOpts
-      return Wallet.fromEncryptedJson(json, password, progressCallback)
-    }
+//       const { json, password, progressCallback } = options.encryptedJsonOpts
+//       return Wallet.fromEncryptedJson(json, password, progressCallback)
+//     }
 
-    return Wallet.createRandom()
-  }
-}
+//     return Wallet.createRandom()
+//   }
+// }
 
-export default EthIdentityProvider
+// export default EthIdentityProvider
