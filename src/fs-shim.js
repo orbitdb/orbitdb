26752f2cfb98611cc9a34dbbfd27853ee80292f0
haad <haad@headbanggames.com>
2023-03-02 07:11:31 +0200

    Delete fs-shim

--- src/fs-shim.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import { isElectronMain } from 'wherearewe'
-import * as fs_ from 'fs'
-
-export const fs = (!isElectronMain && (typeof window === 'object' || typeof self === 'object'))
-  ? null
-  : fs_
-
-export default fs
