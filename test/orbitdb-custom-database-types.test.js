d1e59a95b710543e071e961fc5fb9116727c0757
haad <haad@headbanggames.com>
2023-03-10 11:29:00 +0200

    Make it possible to add custom database types

---
+++ test/orbitdb-custom-database-types.test.js
@@ -0,0 +1,115 @@
+import { strictEqual, deepStrictEqual, notStrictEqual } from 'assert'
+import rmrf from 'rimraf'
+import * as IPFS from 'ipfs'
+import { OrbitDB, addDatabaseType, databaseTypes } from '../src/index.js'
+import config from './config.js'
+
+const type = 'custom!'
+
+const CustomStore = async ({ OpLog, Database, ipfs, identity, address, name, accessController, directory, storage, meta }) => {
+  const database = await Database({ OpLog, ipfs, identity, address, name, accessController, directory, storage, meta })
+
+  return {
+    ...database,
+    type
+  }
+}
+
+describe('Add a custom database type', function () {
+  this.timeout(5000)
+
+  let ipfs
+  let orbitdb
+
+  before(async () => {
+    ipfs = await IPFS.create({ ...config.daemon1, repo: './ipfs1' })
+    orbitdb = await OrbitDB({ ipfs })
+  })
+
+  after(async () => {
+    if (orbitdb) {
+      await orbitdb.stop()
+    }
+
+    if (ipfs) {
+      await ipfs.stop()
+    }
+
+    // Remove the added custom database type from OrbitDB import
+    delete databaseTypes[CustomStore.type]
+
+    await rmrf('./orbitdb')
+    await rmrf('./ipfs1')
+  })
+
+  describe('Default supported database types', function () {
+    it('returns default supported database types', async () => {
+      const expected = [
+        'events',
+        'documents',
+        'keyvalue'
+      ]
+
+      deepStrictEqual(Object.keys(databaseTypes), expected)
+    })
+
+    it('throws and error if custom database type hasn\'t been added', async () => {
+      let err
+      try {
+        await orbitdb.open('hello', { type })
+      } catch (e) {
+        err = e
+      }
+      notStrictEqual(err, undefined)
+      strictEqual(err.message, 'Unspported database type: \'custom!\'')
+    })
+  })
+
+  describe('Custom database type', function () {
+    before(() => {
+      addDatabaseType(type, CustomStore)
+    })
+
+    it('create a database with the custom database type', async () => {
+      const name = 'hello custom database'
+      const db = await orbitdb.open(name, { type })
+      strictEqual(db.type, type)
+      strictEqual(db.name, name)
+    })
+
+    it('throws and error if custom database type already exists', async () => {
+      let err
+      try {
+        addDatabaseType(type, CustomStore)
+        throw new Error('This should not run.')
+      } catch (e) {
+        err = e
+      }
+      notStrictEqual(err, undefined)
+      strictEqual(err.message.indexOf('already exists') !== -1, true)
+    })
+
+    it('returns custom database type after adding it', async () => {
+      const expected = [
+        'events',
+        'documents',
+        'keyvalue',
+        type
+      ]
+
+      deepStrictEqual(Object.keys(databaseTypes), expected)
+    })
+
+    it('can be removed from supported database types', async () => {
+      const expected = [
+        'events',
+        'documents',
+        'keyvalue'
+      ]
+
+      delete databaseTypes[type]
+
+      deepStrictEqual(Object.keys(databaseTypes), expected)
+    })
+  })
+})
