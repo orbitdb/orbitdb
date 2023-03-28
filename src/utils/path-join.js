3c79588778741057cef1cea64f5a774817678051
saul <saul@organicdesign.co.nz>
2023-03-28 13:04:08 +1300

    Fix process undefined error in browsers.

--- src/utils/path-join.js
+++ src/utils/path-join.js
@@ -3,7 +3,7 @@ const win32Reg = /((?<=\\)\\+)|(^\.\\)|((?<=\\)\.\\)/g
 
 const createJoin = isWin => (...paths) => paths.join(isWin ? '\\' : '/').replace(isWin ? win32Reg : posixReg, '')
 
-export const join = createJoin(process?.platform === 'win32')
+export const join = createJoin(typeof process !== 'undefined' && process?.platform === 'win32')
 
 export const posixJoin = createJoin(false)
 export const win32Join = createJoin(true)
