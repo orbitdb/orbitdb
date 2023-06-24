e6f86fa203edb399d688285338440c952addaa8a
Hayden Young <haydenyoung@wijiti.com>
2023-06-24 16:18:46 +0100

    docs: Do not document utils. They are not exposed externally.

--- src/utils/ensure-ac-address.js
+++ src/utils/ensure-ac-address.js
@@ -1,12 +1,5 @@
 import pathJoin from './path-join.js'
 
-/**
- * Checks that the given address has '/_access' as the last part.
- * @function
- * @param {string} address The address to check.
- * @return {string} The address appended with /_access.
- * @memberof module:Utils
- */
 export default address => {
   const suffix = address.toString().split('/').pop()
   return suffix === '_access'
