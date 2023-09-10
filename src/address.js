a928dca6f8d9ddfb8c0ea2aa25ff79fea2cec85d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-09-10 15:19:43 +1000

    chore: add prettier settings

--- src/address.js
+++ src/address.js
@@ -14,26 +14,26 @@ import { posixJoin } from './utils/path-join.js'
  * false otherwise.
  * @static
  */
-const isValidAddress = (address) => {
-  address = address.toString()
+const isValidAddress = address => {
+	address = address.toString()
 
-  if (!address.startsWith('/orbitdb') && !address.startsWith('\\orbitdb')) {
-    return false
-  }
+	if (!address.startsWith('/orbitdb') && !address.startsWith('\\orbitdb')) {
+		return false
+	}
 
-  address = address.replaceAll('/orbitdb/', '')
-  address = address.replaceAll('\\orbitdb\\', '')
-  address = address.replaceAll('/', '')
-  address = address.replaceAll('\\', '')
+	address = address.replaceAll('/orbitdb/', '')
+	address = address.replaceAll('\\orbitdb\\', '')
+	address = address.replaceAll('/', '')
+	address = address.replaceAll('\\', '')
 
-  let cid
-  try {
-    cid = CID.parse(address, base58btc)
-  } catch (e) {
-    return false
-  }
+	let cid
+	try {
+		cid = CID.parse(address, base58btc)
+	} catch (e) {
+		return false
+	}
 
-  return cid !== undefined
+	return cid !== undefined
 }
 
 /**
@@ -45,16 +45,16 @@ const isValidAddress = (address) => {
  * @throws Not a valid OrbitDB address if address is invalid.
  * @static
  */
-const parseAddress = (address) => {
-  if (!address) {
-    throw new Error(`Not a valid OrbitDB address: ${address}`)
-  }
+const parseAddress = address => {
+	if (!address) {
+		throw new Error(`Not a valid OrbitDB address: ${address}`)
+	}
 
-  if (!isValidAddress(address)) {
-    throw new Error(`Not a valid OrbitDB address: ${address}`)
-  }
+	if (!isValidAddress(address)) {
+		throw new Error(`Not a valid OrbitDB address: ${address}`)
+	}
 
-  return OrbitDBAddress(address)
+	return OrbitDBAddress(address)
 }
 
 /**
@@ -63,30 +63,30 @@ const parseAddress = (address) => {
  * @property {string} hash The hash of the database manifest.
  * @property {string} address The full database address.
  */
-const OrbitDBAddress = (address) => {
-  if (address && address.protocol === 'orbitdb' && address.hash) {
-    return address
-  }
+const OrbitDBAddress = address => {
+	if (address && address.protocol === 'orbitdb' && address.hash) {
+		return address
+	}
 
-  const protocol = 'orbitdb'
+	const protocol = 'orbitdb'
 
-  const hash = address.replace('/orbitdb/', '').replace('\\orbitdb\\', '')
+	const hash = address.replace('/orbitdb/', '').replace('\\orbitdb\\', '')
 
-  /**
-   * Returns address as a string.
-   * @typedef {Function} toString
-   * @returns {string} Address as a string.
-   */
-  const toString = () => {
-    return posixJoin('/', protocol, hash)
-  }
+	/**
+	 * Returns address as a string.
+	 * @typedef {Function} toString
+	 * @returns {string} Address as a string.
+	 */
+	const toString = () => {
+		return posixJoin('/', protocol, hash)
+	}
 
-  return {
-    protocol,
-    hash,
-    address,
-    toString
-  }
+	return {
+		protocol,
+		hash,
+		address,
+		toString,
+	}
 }
 
 export { OrbitDBAddress as default, isValidAddress, parseAddress }
