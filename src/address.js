716fd56fcbb2c1d0dd07b290cfb2e7f690c97e63
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-09-10 15:30:30 +1000

    chore: updating linting settings

--- src/address.js
+++ src/address.js
@@ -15,25 +15,25 @@ import { posixJoin } from './utils/path-join.js'
  * @static
  */
 const isValidAddress = (address) => {
-	address = address.toString()
+  address = address.toString()
 
-	if (!address.startsWith('/orbitdb') && !address.startsWith('\\orbitdb')) {
-		return false
-	}
+  if (!address.startsWith('/orbitdb') && !address.startsWith('\\orbitdb')) {
+    return false
+  }
 
-	address = address.replaceAll('/orbitdb/', '')
-	address = address.replaceAll('\\orbitdb\\', '')
-	address = address.replaceAll('/', '')
-	address = address.replaceAll('\\', '')
+  address = address.replaceAll('/orbitdb/', '')
+  address = address.replaceAll('\\orbitdb\\', '')
+  address = address.replaceAll('/', '')
+  address = address.replaceAll('\\', '')
 
-	let cid
-	try {
-		cid = CID.parse(address, base58btc)
-	} catch (e) {
-		return false
-	}
+  let cid
+  try {
+    cid = CID.parse(address, base58btc)
+  } catch (e) {
+    return false
+  }
 
-	return cid !== undefined
+  return cid !== undefined
 }
 
 /**
@@ -46,15 +46,15 @@ const isValidAddress = (address) => {
  * @static
  */
 const parseAddress = (address) => {
-	if (!address) {
-		throw new Error(`Not a valid OrbitDB address: ${address}`)
-	}
+  if (!address) {
+    throw new Error(`Not a valid OrbitDB address: ${address}`)
+  }
 
-	if (!isValidAddress(address)) {
-		throw new Error(`Not a valid OrbitDB address: ${address}`)
-	}
+  if (!isValidAddress(address)) {
+    throw new Error(`Not a valid OrbitDB address: ${address}`)
+  }
 
-	return OrbitDBAddress(address)
+  return OrbitDBAddress(address)
 }
 
 /**
@@ -64,29 +64,29 @@ const parseAddress = (address) => {
  * @property {string} address The full database address.
  */
 const OrbitDBAddress = (address) => {
-	if (address && address.protocol === 'orbitdb' && address.hash) {
-		return address
-	}
+  if (address && address.protocol === 'orbitdb' && address.hash) {
+    return address
+  }
 
-	const protocol = 'orbitdb'
+  const protocol = 'orbitdb'
 
-	const hash = address.replace('/orbitdb/', '').replace('\\orbitdb\\', '')
+  const hash = address.replace('/orbitdb/', '').replace('\\orbitdb\\', '')
 
-	/**
-	 * Returns address as a string.
-	 * @typedef {Function} toString
-	 * @returns {string} Address as a string.
-	 */
-	const toString = () => {
-		return posixJoin('/', protocol, hash)
-	}
+  /**
+   * Returns address as a string.
+   * @typedef {Function} toString
+   * @returns {string} Address as a string.
+   */
+  const toString = () => {
+    return posixJoin('/', protocol, hash)
+  }
 
-	return {
-		protocol,
-		hash,
-		address,
-		toString,
-	}
+  return {
+    protocol,
+    hash,
+    address,
+    toString,
+  }
 }
 
 export { OrbitDBAddress as default, isValidAddress, parseAddress }
